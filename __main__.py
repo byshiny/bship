d8ff009eb26e0538b49c1126b8a60864d4ec4a22
byshiny <bs5ht@virginia.edu>
2018-10-29 22:55:42 -0400

    bship to be continued

--- __main__.py
+++ __main__.py
@@ -1,10 +1,46 @@
-
 from grid import Grid
+from grid import ShipTypes
+from grid import Ship
+from random import randint
+import sys
 def main():
-    grid = Grid()
-    grid.print_grid()
 
+    #print welcome message
+
+    #decide between 1 player or 2 player
+
+    #if 2 player: instruct players to modify the text files while they are not looking
+
+    grid = Grid()
+    grid.print_hit_grid()
+    max_cols = grid.get_col_max()
+    max_rows = grid.get_row_max()
+    r = randint(0, max_cols)
+    c = randint(0, max_rows)
+    direction = randint(0, 4)
+    ship_left = r
+    ship_right = r
+    ship_up = c
+    ship_down = c
+
+    ship_list = [ShipTypes.CARRIER]
+    if direction == 0:
+        ship_left = r - ship_list[0].value
+    elif direction == 1:
+        ship_right = r + ship_list[0].value
+    elif direction == 2:
+        ship_up = c - ship_list[0].value
+    else:
+        ship_down = c + ship_list[0].value
+    print(ship_left)
+    print(ship_right)
+    print(ship_up)
+    print(ship_down)
+    withinRange = grid.is_within_grid_range(ship_left, ship_right, ship_up, ship_down)
+    ship = Ship(ship_list[0], ship_left, ship_right, ship_up, ship_down)
+    print(withinRange)
+    if(withinRange):
+        grid.place_ship(ship)
+    grid.print_hit_grid()
 if __name__ == '__main__':
     main()
-
-
--- grid.py
+++ grid.py
@@ -1,11 +1,49 @@
 
 #self class represents the current world
 import copy
+from enum import Enum
+class ShipTypes(Enum):
+    CARRIER = 5
+    BATTLESHIP = 4
+    SUBMARINE = 3
+    CRUISER = 3
+    DESTROYER = 2
+class Ship:
+    ship_left = 0
+    ship_right = 0
+    ship_up = 0
+    ship_down = 0
+    def __init__(self, r, c):
+        """Creates a default ship of length 1
+        Args:
+            r, c - row and column
+        Return:
+            ship
+        """
+        self.ship_left = r
+        self.ship_right = r
+        self.ship_up = c
+        self.ship_down = c
+        self.ship_type = ShipTypes.DESTROYER
+    def __init__(self, ship_type, ship_left, ship_right, ship_up, ship_down):
+        """Creates a default ship of length 1
+        Args:
+            r, c - row and column
+        Return:
+            ship
+        """
+        self.ship_type = ship_type
+        self.ship_left = ship_left
+        self.ship_right = ship_right
+        self.ship_up = ship_up
+        self.ship_down = ship_down
+
 class Grid:
-    
+
     row_max = 10
     col_max = 10
-    grid = None
+    hit_grid = None
+    ship_grid = None
     def __init__(self):
         """Creates a default 10x10 matrix grid
         Args:
@@ -15,7 +53,8 @@ class Grid:
         """
         self.row_max = 10
         self.col_max = 10
-        self.grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)] 
+        self.hit_grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)]
+        self.ship_grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)]
 
     def get_row_max(self):
         """Returns maximum row size of the grid
@@ -35,9 +74,54 @@ class Grid:
         """
         return self.row_max;
 
-    def print_grid(self):
+    def is_within_grid_range(self, r, c):
+        """Checks if a given row and column are within the grid's range
+        Args:
+            row, column
+        Return:
+            True or false
+        """
+        if r >= 0 and r < self.row_max and c >=0 and c < self.col_max:
+            return True
+        return False
+    def place_ship(self, ship):
+        """Places the ship on the grid
+        Args:
+            Ship
+        Return:
+            True if the ship has successfully been placed
+        """
+        print(ship)
+        if ship.ship_left == ship.ship_right:
+            col = ship.ship_left
+            for r in xrange(ship.ship_up, ship.ship_down, 1):
+                self.hit_grid[r][col] = 1
+                self.ship_grid[r][col] = ship.ship_type
+        else:
+            row = ship.ship_up
+            for c in xrange(ship.ship_left, ship.ship_right, 1):
+                self.hit_grid[row][c] = 1
+                self.ship_grid[r][col] = ship.ship_type
+        return True
+    def is_within_grid_range(self, left_r, right_r, up_c ,down_c):
+        """Checks if a given row and column are within the grid's range
+        Args:
+            left_row right_row left_column, right_column
+        Return:
+            True or false
+        """
+        if left_r >= 0 and left_r < self.row_max and up_c >=0 and up_c < self.col_max and \
+            right_r >= 0 and right_r < self.row_max and down_c >=0 and down_c < self.col_max:
+            return True
+        return False
+
+    def print_hit_grid(self):
+        """Prints the places that have been hit, which are marked by one
+        Args:
+            None
+        """
         for row in xrange(0, self.row_max, 1):
             row_line = ""
             for col in xrange(0, self.col_max, 1):
-                row_line += str(self.grid[row][col]) + " "
+                row_line += str(self.hit_grid[row][col]) + " "
             print(row_line)
