3ac4eebe2657c02c7a2ae3f48491aefe0b75d70b
byshiny <bs5ht@virginia.edu>
2018-10-31 00:17:47 -0400

    finished battleship

--- __main__.py
+++ __main__.py
@@ -9,14 +9,29 @@ def main():
 
     #decide between 1 player or 2 player
 
-    #if 2 player: instruct players to modify the text files while they are not looking
-
-    grid = Grid()
-    grid.print_hit_grid()
-    
+    #if 2 player: instruct players to modify the text files while they are not looking - to be implemented
+    turns = 0
+    grid = Grid()  
     ship_list = [ShipTypes.CARRIER, ShipTypes.BATTLESHIP, ShipTypes.CRUISER, ShipTypes.DESTROYER, ShipTypes.SUBMARINE]
     generate_ships_on_a_grid(grid, ship_list)
+    print("Welcome to battleship! Your grid size is" + "r: " + str(grid.row_max) + "," +  "c:" + str(grid.col_max) )
+    print("Comrade you need to sink 5 ships of 5, 4, 3, 3, 2. The game begins NOW!")
     grid.print_hit_grid()
+    grid.print_player_grid()
+    while(grid.total_hits_left > 0):
+        try:
+            r,c = input('Coordinates please (with a comma in between, -1, -1 to quit =( ): ')
+            if r == -1 and c == -1:
+                grid.total_hits_left = -9000
+                print("Turns taken: " + str(turns))
+                #print("Ships sunk: " + )
+            else:
+                grid.is_point_within_grid_range(r,c)
+                grid.evaluate_hit(r,c)
+                turns +=1
+        except:
+            print("You probably made an input error. Try again")
+    print("Congratulations! You destroyed all the ships! You took " + str(turns) + " Turns.")
 
 def generate_ships_on_a_grid(grid, ship_list):
     """Generates ships on a grid by updating the hit grid 
@@ -28,7 +43,7 @@ def generate_ships_on_a_grid(grid, ship_list):
     max_cols = grid.get_col_max()
     max_rows = grid.get_row_max()
    
-    
+    ship_id = 0
     while(len(ship_list) > 0):
         r = randint(0, max_cols)
         c = randint(0, max_rows)
@@ -49,14 +64,13 @@ def generate_ships_on_a_grid(grid, ship_list):
             ship_down = c + ship_list[0].value
         
         withinRange = False
-        withinRange = grid.is_within_grid_range(ship_left, ship_right, ship_up, ship_down)
-        
-        ship = Ship(ship_list[0], ship_left, ship_right, ship_up, ship_down)
-        print(withinRange)
+        withinRange = grid.is_four_coords_within_grid_range(ship_left, ship_right, ship_up, ship_down)
         if withinRange:
             avaliable = grid.avaliable_to_place(ship_left, ship_right, ship_up, ship_down)
             if avaliable:
+                ship = Ship(ship_list[0], ship_left, ship_right, ship_up, ship_down, ship_id)
                 grid.place_ship(ship)
+                ship_id +=1
                 del ship_list[0]
 
 
--- grid.py
+++ grid.py
@@ -13,6 +13,8 @@ class Ship:
     ship_right = 0
     ship_up = 0
     ship_down = 0
+    ship_life = 0
+    
     def __init__(self, r, c):
         """Creates a default ship of length 1
         Args:
@@ -24,8 +26,9 @@ class Ship:
         self.ship_right = r
         self.ship_up = c
         self.ship_down = c
-        self.ship_type = ShipTypes.DESTROYER
-    def __init__(self, ship_type, ship_left, ship_right, ship_up, ship_down):
+        self.ship_id = -1
+
+    def __init__(self, ship_type, ship_left, ship_right, ship_up, ship_down, id):
         """Creates a default ship of length 1
         Args:
             r, c - row and column
@@ -37,13 +40,21 @@ class Ship:
         self.ship_right = ship_right
         self.ship_up = ship_up
         self.ship_down = ship_down
+        self.ship_id = id
+        self.ship_life = ship_type.value
 
 class Grid:
-
+    ship_label_incrementer = 0
     row_max = 10
     col_max = 10
     hit_grid = None
     ship_grid = None
+    player_grid = None
+    # keeps track of ship lifespan
+    ship_life_dict = {}
+    ship_obj_dict = {}
+    total_hits_left = 0
+    ships_remaining = 0
     def __init__(self):
         """Creates a default 10x10 matrix grid
         Args:
@@ -55,6 +66,7 @@ class Grid:
         self.col_max = 10
         self.hit_grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)]
         self.ship_grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)]
+        self.player_grid = [[0 for x in range(self.col_max)] for y in range(self.row_max)]
 
     def get_row_max(self):
         """Returns maximum row size of the grid
@@ -74,7 +86,7 @@ class Grid:
         """
         return self.row_max;
 
-    def is_within_grid_range(self, r, c):
+    def is_point_within_grid_range(self, r, c):
         """Checks if a given row and column are within the grid's range
         Args:
             row, column
@@ -91,17 +103,22 @@ class Grid:
         Return:
             True if the ship has successfully been placed
         """
-        print(ship)
+        ship_size = 0
         if ship.ship_left == ship.ship_right:
             col = ship.ship_left
             for r in xrange(ship.ship_up, ship.ship_down, 1):
                 self.hit_grid[r][col] = 1
-                self.ship_grid[r][col] = ship.ship_type
+                self.ship_grid[r][col] = ship
+                
         else:
             row = ship.ship_up
             for c in xrange(ship.ship_left, ship.ship_right, 1):
                 self.hit_grid[row][c] = 1
-                self.ship_grid[row][c] = ship.ship_type
+                self.ship_grid[row][c] = ship
+                
+        self.ship_life_dict[ship.ship_id] = ship.ship_life
+        self.total_hits_left += ship.ship_life
+        self.ships_remaining +=1
         return True
 
     def avaliable_to_place(self, ship_left, ship_right, ship_up, ship_down):
@@ -111,7 +128,6 @@ class Grid:
         Return:
             True if the ship can be places
         """
-        print()
         if ship_left == ship_right:
             col = ship_left
             for r in xrange(ship_up, ship_down, 1):
@@ -124,7 +140,7 @@ class Grid:
                     return False
         return True  
 
-    def is_within_grid_range(self, left_r, right_r, up_c ,down_c):
+    def is_four_coords_within_grid_range(self, left_r, right_r, up_c ,down_c):
         """Checks if a given row and column are within the grid's range
         Args:
             left_row right_row left_column, right_column
@@ -136,13 +152,75 @@ class Grid:
             return True
         return False
 
+    def print_grid_helper(self, grid_to_print):
+        print " ",
+        for col in xrange(0, self.col_max, 1):
+            print col,
+        print 
+        print " ",
+        for col in xrange(0, self.col_max, 1):
+            print '_',
+        print
+        for row in xrange(0, self.row_max, 1):
+            row_line = str(row) + "|"
+            for col in xrange(0, self.col_max, 1):
+                row_line += str(grid_to_print[row][col]) + " "
+            print(row_line)
+        print 
+
     def print_hit_grid(self):
         """Prints the places that have been hit, which are marked by one
         Args:
             None
         """
-        for row in xrange(0, self.row_max, 1):
-            row_line = ""
-            for col in xrange(0, self.col_max, 1):
-                row_line += str(self.hit_grid[row][col]) + " "
-            print(row_line)
+        self.print_grid_helper(self.hit_grid)
+    
+    
+
+    def print_ship_grid(self):
+        """Prints the grid that has the ship labels
+        Args:
+            None
+        """
+        self.print_grid_helper(self.ship_grid)
+
+    def print_player_grid(self):
+        """Prints the grid that has the ship labels
+        Args:
+            None
+        """
+        self.print_grid_helper(self.player_grid)
+
+    def evaluate_hit(self, r, c):
+        """Prints the grid that has the ship labels
+        Args:
+            None
+        """
+        self.player_grid[r][c] = 1
+        self.print_player_grid()
+        hit = 0
+        #ship has not yet been hit
+        if self.hit_grid[r][c] == -1:
+            print("You already hit that coordinate. Try another coordinate")
+            return -1
+
+        if self.hit_grid[r][c] == 1:
+            hit = 1
+            self.hit_grid[r][c] = -1
+            ship = self.ship_grid[r][c]
+            self.ship_life_dict[ship.ship_id] -=1
+            if(self.ship_life_dict[ship.ship_id] ) <= 0:
+                ship = self.ship_grid[r][c]
+                self.ship_life_dict[ship.ship_id] -=1
+                if(self.ship_life_dict[ship.ship_id] <=0):
+                    print("Woot! You've sunk a ship! You sank " + str(ship.ship_type.name))
+            else:
+                print("Success! you've hit a ship!")
+            self.total_hits_left -=1
+            
+        else:
+            print("Haha you missed! Try again good sir")
+        self.hit_grid[r][c] = -1
+        
+        return hit
+
